301b5d752ce53c7f39bfee951c5f40facdb1fd7a
张逸帆 <scirocco_gti@yeah.net>
2022-12-16 23:55:45 +0800

    feat: leatcode editor font-family and GitLab font-szie

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 ## LeetCode font-size
 
 力扣题目描述字体实在太小了，这里手动把他改到 `16px`
+
+## LeetCode font-family
+
+手动把力扣编辑器字体改为 `等距更纱黑体`
+
+## GitLab font-size
+
+手动改大 GitLab 代码窗口字体
--- Sciroccogti-CSS.user.js
+++ Sciroccogti-CSS.user.js
@@ -1,23 +1,38 @@
 // ==UserScript==
 // @name            Sciroccogti-CSS
-// @version         0.1
+// @version         0.2
 // @description     个人的CSS样式脚本
 // @updateURL       https://raw.githubusercontent.com/Sciroccogti/myCSSScripts/master/dist/Sciroccogti-CSS.user.js
 // @downloadURL     https://raw.githubusercontent.com/Sciroccogti/myCSSScripts/master/dist/Sciroccogti-CSS.user.js
 // @author          Sciroccogti
 // @license         MIT
 // @match           *://*leetcode.cn/*
+// @match           *://git.lab.sciroccogti.top/*
+// @match           *://gitlab.seu.edu.cn/*
 // @icon            https://www.sciroccogti.top/img/favicon.ico
 // @grant           GM_addStyle
+// @run-at          document-end
 // ==/UserScript==
 
 (function() {
     'use strict';
     // 力扣加大字体
     let leetcode_css =`
+        /* leetcode problem description */
         .content__1Y2H {
-            font-size:16px
+            font-size:1rem;
         }
-       `
+
+        /* leetcode editor */
+        .monaco-mouse-cursor-text {
+            font-family: Sarasa Mono SC Nerd !important;
+        }
+
+        /* gitlab code */
+        .file-content.code pre .line {
+            font-size:14px !important;
+            line-height:21px !important;
+        }
+        `
     GM_addStyle(leetcode_css)
 })();
